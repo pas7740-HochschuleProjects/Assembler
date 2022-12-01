514f8f71ca4e50166b49abc971ab8bbd88eb41d1
PascalSchwab <blazerclips@gmail.com>
2022-12-01 11:41:17 +0100

    Testat

--- 1. Testat/main.asm
+++ 1. Testat/main.asm
@@ -1,8 +1,3 @@
-.include "io.asm"
-
-# Berechnung:
-# A < b
-
 .data
 	firstInputText: .asciiz "First Word (max. 10 Characters): "
 	secondInputText: .asciiz "Second Word (max. 10 Characters): "
@@ -12,6 +7,31 @@
 	inputSize: .word 11
 
 .text
+	# Prints string in console / %string needs address
+    	.macro PrintString (%string)
+    		la $a0, %string
+    		li $v0, 4	# Syscall "Print string"
+    		syscall
+    	.end_macro
+    	
+    	# Reads console input as string / %buffer needs an address, %size needs a word
+    	.macro ReadStringInput (%buffer, %size)
+		la $a0, %buffer
+		lw $a1, %size
+		li $v0, 8	# Syscall "Read string"
+		syscall
+	.end_macro
+	
+	# Exit Program
+	.macro Exit
+		li $v0, 10
+             	syscall
+        .end_macro
+
+	#--------------------------------------
+	#------------ Start Program -----------
+	#--------------------------------------
+
 	# Print Text and Read Input
 	PrintString(firstInputText)
 	ReadStringInput(firstInput, inputSize)
